8bdb8cf6fd70e70d9c6edfb26c133aea0136291d
agrim106 <agrimchaudhary2@gmail.com>
2025-01-04 21:09:35 +0530

    [Agrim]:Added a file on nested collections.

---
+++ nestedCollections.rb
@@ -0,0 +1,13 @@
+#An example of a nested or a multidimensional array 
+test_scores = [[12,11,32,50,34,33,44,43,23,29] , [7,17,71,45,65,23] , [89,98,20,65,56,78,99] ]
+#this a nested array 
+teacher_mailboxes = [["marco", "polo" , "genghis","khan","romeo","princip","prateek"] ,  ["Jones", "Lewis", "Lopez", "Moore"],
+["Perez", "Scott", "Smith", "Young"]]
+
+puts test_scores
+puts teacher_mailboxes[0][0]
+
+puts teacher_mailboxes[1][0]
+
+puts teacher_mailboxes[2][0]
+
